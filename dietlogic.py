406f4a398ff29cec40499d8d9048eacfc5b1c09f
Shreesh Upadhayay <shreesh.upa2007@gmail.com>
2026-03-31 19:41:02 +0530

    added main diet logic

---
+++ dietlogic.py
@@ -0,0 +1,36 @@
+def calories(weight, goal):
+    if goal == "loss":
+        g=weight*25
+        return g
+    elif goal == "gain":
+        g=weight*35
+        return g
+    else:
+        g=weight*30
+        return
+
+
+def diet(goal):
+    if goal == "loss":
+        return {
+            "Breakfast": "Oats + Fruits",
+            "Lunch": "Brown rice + Dal + Vegetables",
+            "Dinner": "2 Chapati + Sabzi",
+            "Snacks": "Nuts / Green Tea"
+        }
+
+    elif goal == "gain":
+        return {
+            "Breakfast": "Milk + Banana + Peanut Butter",
+            "Lunch": "Rice + Paneer/Chicken",
+            "Dinner": "Chapati + Eggs/Paneer",
+            "Snacks": "Dry fruits + Smoothie"
+        }
+
+    else:
+        return {
+            "Breakfast": "Poha/Upma + Fruits",
+            "Lunch": "Rice + Dal + Vegetables",
+            "Dinner": "Chapati + Sabzi",
+            "Snacks": "Fruits / Nuts"
+        }
